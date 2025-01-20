25deb848d3627793af1d85e6410ce2b15f56c5f8
Christopher Wade <chriswade@Christophers-MacBook-Air.local>
2025-01-20 09:28:45 -0800

    Add iOS build configuration and fixes

---
+++ Archivo/Archivo/ArchivoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchivoApp.swift
+//  Archivo
+//
+//  Created by Christopher Wade on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ArchivoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
